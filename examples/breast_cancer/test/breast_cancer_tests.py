8423f388ad34cbeb5e6cb07a3b4497a8fa154d30
Alex Cabrera <alex.cabrera@gmail.com>
2022-06-29 11:59:06 -0400

    update cancer files

--- examples/breast_cancer/test/breast_cancer_tests.py
+++ examples/breast_cancer/test/breast_cancer_tests.py
@@ -1,28 +1,29 @@
-import torchvision.transforms as transforms
-import PIL
-import torch
 import os
-from zeno import load_data, load_model, metric
-from examples.breast_cancer.vgg_old import vgg16_bn
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
+import torchvision.transforms as transforms
+from examples.breast_cancer.vgg_old import vgg16_bn
+from PIL import Image
+from sklearn.metrics import f1_score, recall_score
+from zeno import ZenoOptions, metric_function, predict_function
 
 transform_image = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
 )
 
-classes = (
-    "normal",
-    "lesion"
-)
+classes = ("normal", "lesion")
 
 
 num_classes = 2
 image_res = 512
 transform_image = transforms.Compose(
-    [transforms.Resize(image_res), transforms.ToTensor(),
-])
+    [
+        transforms.Resize(image_res),
+        transforms.ToTensor(),
+    ]
+)
 
 
 # Type 1
@@ -61,12 +62,12 @@ transform_image = transforms.Compose(
 #     return pred
 
 # Type 3
-@load_model
+@predict_function
 def load_model(model_path):
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = vgg16_bn(pretrained=False)
     model.classifier.fc8a = nn.Linear(model.classifier.fc8a.in_features, num_classes)
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
 
     # a dict to store the activations
     activation = {}
@@ -75,29 +76,38 @@ def load_model(model_path):
         # the hook signature
         def hook(model, input, output):
             activation[name] = output.detach()
+
         return hook
 
     # register forward hooks on the layers of choice
-    h = model.classifier.fc7.register_forward_hook(getActivation('emb'))
+    h = model.classifier.fc7.register_forward_hook(getActivation("emb"))
 
-    def pred(instances):
-        imgs = torch.stack([transform_image(img) for img in instances], dim=0)
+    def pred(df, ops: ZenoOptions):
+        imgs = torch.stack(
+            [
+                transform_image(Image.open(os.path.join(ops.data_path, img)))
+                for img in df[ops.data_column]
+            ],
+            dim=0,
+        )
         prob = []
         emb = None
         with torch.no_grad():
             for img in imgs:
                 out = model(img.unsqueeze(0))
                 if emb is None:
-                    emb = activation['emb']
+                    emb = activation["emb"]
                 else:
-                    emb = torch.cat((emb, activation['emb']), 0)
+                    emb = torch.cat((emb, activation["emb"]), 0)
                 p = F.softmax(out, dim=1)
                 index = (torch.argmax(p, dim=1).detach().numpy())[0]
                 prob.append(classes[index])
         h.remove()
-        return prob, emb.detach().numpy()    
-        #batches 
+        return prob, emb.detach().numpy()
+        # batches
+
     return pred
+
 
 # @load_model
 # def load_model(model_path):
@@ -129,12 +139,18 @@ def load_model(model_path):
 #     return pred
 
 
-@load_data
-def load_data(df_metadata, data_path):
-    return [PIL.Image.open(os.path.join(data_path, img)) for img in df_metadata.index]
+@metric_function
+def accuracy(df, ops: ZenoOptions):
+    if len(df) == 0:
+        return 0
+    return 100 * (df[ops.label_column] == df[ops.output_column]).sum() / len(df)
 
 
-@metric
-def accuracy(output, metadata, label_col):
-    # print("start", metadata[label_col]["label"], output, "end")
-    return metadata[label_col] == output
+@metric_function
+def recall(df, ops: ZenoOptions):
+    return 100 * recall_score(df[ops.label_column], df[ops.output_column])
+
+
+@metric_function
+def f1(df, ops: ZenoOptions):
+    return 100 * f1_score(df[ops.label_column], df[ops.output_column])
--- examples/breast_cancer/test/brightness.py
+++ examples/breast_cancer/test/brightness.py
@@ -1,24 +1,15 @@
-import PIL
-from zeno import slicer, transform, preprocess
+from PIL import Image, ImageStat
+from zeno import distill_function
+import os
 
 # Ref https://stackoverflow.com/questions/3490727/what-are-some-methods-to-analyze-image-brightness-using-python
 def get_brightness(im):
-    im = im.convert('L')
-    stat = PIL.ImageStat.Stat(im)
+    im = im.convert("L")
+    stat = ImageStat.Stat(im)
     return stat.mean[0]
 
 
-@preprocess
-def brightness(images):
-    return [get_brightness(im) for im in images]
-
-
-@slicer
-def low_exposure(metadata):
-    return metadata[metadata["brightness"] < 50].index
-
-
-@slicer
-def high_exposure(metadata):
-    return metadata[metadata["brightness"] > 90].index
-
+@distill_function
+def brightness(df, ops):
+    imgs = [Image.open(os.path.join(ops.data_path, img)) for img in df[ops.data_column]]
+    return [get_brightness(im) for im in imgs]
