fe7d3b2d9dfa989af6aced9414f6ed8ff467e773
xnought <bertuccd@oregonstate.edu>
2022-07-06 16:41:40 -0400

    feat: add parametric to executable api

---
+++ zeno/pipeline/parametric_umap.py
@@ -0,0 +1,30 @@
+from umap.parametric_umap import (  # type: ignore
+    load_ParametricUMAP,  # type: ignore
+    ParametricUMAP,  # type: ignore
+)  # type: ignore
+
+from .executable_api import Executable
+
+
+class ParametricUMAPNode(Executable):
+    def __init__(self):
+        self.status = ""
+
+    def execute(self, input):
+        self.projections = self.model.fit_transform(input)
+        self.input = input
+
+    def pipe_outputs(self):
+        return {"input": self.input, "projection2D": self.projections}
+
+    def export_outputs_js(self):
+        return {"projection2D": self.projections}
+
+    def save_executable(self, path: str):
+        self.model.save(path)
+
+    def load_executable(self, path: str):
+        self.model = load_ParametricUMAP(path)
+
+    def new_executable(self, *args, **kwargs):
+        self.model = ParametricUMAP(*args, **kwargs)
